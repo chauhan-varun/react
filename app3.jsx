31ee4f9edf23b8494cdb9cb68438ba342ff9be1a
Varun chauhan <115783538+chauhan-varun@users.noreply.github.com>
2025-02-17 18:14:07 +0530

    useRef

---
+++ app3.jsx
@@ -0,0 +1,17 @@
+import { useRef } from "react";
+
+function App(){
+    const inputRef = useRef();
+    function handelRef(){
+        inputRef.current.focus();
+    }
+    return (
+        <div>
+            <div>Sign Up</div>
+                <input ref={inputRef} type="text" placeholder="username" />
+                <input type="password" placeholder="password" />
+                <button onClick={handelRef} >Sign Up</button>
+            </div>
+    )
+}
+export default App
